06ff0e1363f38da7eec748addd6703d1b506d0b7
ianwitecki <iwitecki@trinity.edu>
2017-12-02 13:40:47 -0600

    obstacleGenerator made

--- Assets/Scripts/CaveGenerator.cs
+++ Assets/Scripts/CaveGenerator.cs
@@ -8,24 +8,51 @@ public partial class CaveGenerator : MonoBehaviour {
 
 	int tunnelDepth = 100;
 
+    private float radius;
+
 	// Use this for initialization
 	void Start () {
-	
-		caveTorus = Resources.Load<GameObject> ("Prefabs/CaveTorus");
+        
+        // Instantiate Rock Obstacles
+        rock1 = Resources.Load<GameObject>("Prefabs/rock1");
+        rock2 = Resources.Load<GameObject>("Prefabs/rock2");
+        rock3 = Resources.Load<GameObject>("Prefabs/rock3");
+        rock4 = Resources.Load<GameObject>("Prefabs/rock4");
+        rock5 = Resources.Load<GameObject>("Prefabs/rock5");
+
+        obstacles = new List<GameObject> { rock1, rock2, rock3, rock4, rock5 };
+
+        caveTorus = Resources.Load<GameObject> ("Prefabs/CaveTorus");
+
+        radius = 5;
 
 		GenerateCave ();
 
 	}
 
 	void GenerateCave() {
+        
+        int obstacleFreq = 2;
 
 		for (int y = 0; y < tunnelDepth; y+=5) {
 
 			float x = Mathf.PerlinNoise(y/40f, 0) * 10f;
-			float z = Mathf.PerlinNoise(y/40f, 0) * 10f; 
+			float z = Mathf.PerlinNoise(y/40f, 0) * 10f;
+
+            //Create Walls
+            Instantiate (caveTorus, new Vector3 (x, y, z), Quaternion.identity);
 
 
-			Instantiate (caveTorus, new Vector3 (x, y, z), Quaternion.identity);
+
+            //Create Obstacles
+            if (obstacleFreq == 0)
+            {
+                ObstacleGenerate(x, y, z);
+                obstacleFreq = 2;
+            } else
+            {
+                obstacleFreq -= 1;
+            }
 
 		}
 
--- Assets/Scripts/ObstacleGenerator.cs
+++ Assets/Scripts/ObstacleGenerator.cs
@@ -4,7 +4,35 @@ using UnityEngine;
 
 public partial class CaveGenerator : MonoBehaviour {
 
-	void ObstacleGenerate(float _x, float _y, float _z) {
-	}
+    //Rock Prefabs
+    private GameObject rock1;
+    private GameObject rock2;
+    private GameObject rock3;
+    private GameObject rock4;
+    private GameObject rock5;
+
+
+    //List of Rock Obstacles
+    private List<GameObject> obstacles;
+
+    void ObstacleGenerate(float _x, float _y, float _z) {
+        int rockChoice = Random.Range(0, 5);
+        GameObject rock = obstacles[rockChoice];
+
+        float theta = Random.Range(0, 2 * Mathf.PI); //angle to instantiate rock at 
+
+        //Spot against wall to Instantiate object at
+        float x_wall = Mathf.Cos(theta) * radius;  
+        float z_wall = Mathf.Sin(theta) * radius;
+
+        Quaternion rotation = Quaternion.identity;
+
+        rotation.eulerAngles = new Vector3(0, (theta*180f)/Mathf.PI , 0);
+        Instantiate(rock, new Vector3(_x + x_wall, _y, _z + z_wall), rotation);
+        
+
+
+
+    }
 
 }
