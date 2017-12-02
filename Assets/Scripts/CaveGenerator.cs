1e544f109147a53c77859d1095133a99712cf08c
ianwitecki <iwitecki@trinity.edu>
2017-12-02 16:53:48 -0600

    level work

--- Assets/Scripts/CaveGenerator.cs
+++ Assets/Scripts/CaveGenerator.cs
@@ -6,7 +6,16 @@ public partial class CaveGenerator : MonoBehaviour {
 
 	private GameObject caveTorus;
 
-	int tunnelDepth = 100;
+
+
+    //Level Variables
+    int tunnelDepth = 2000;
+    int obstacleMinFreq;
+    int obstacleMaxFreq;
+    float doubleChance;
+    float gapChance;
+    int maxGapLength;
+    static int level;
 
 
     private float radius;
@@ -14,7 +23,13 @@ public partial class CaveGenerator : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-        
+        level = 1;
+        obstacleMinFreq = 15;
+        obstacleMaxFreq = 5;
+        doubleChance = 5;
+        gapChance = 5;
+        maxGapLength = obstacleMinFreq + 10;
+  
         // Instantiate Rock Obstacles
         rock1 = Resources.Load<GameObject>("Prefabs/rock1Parent");
         rock2 = Resources.Load<GameObject>("Prefabs/rock2Parent");
@@ -33,16 +48,16 @@ public partial class CaveGenerator : MonoBehaviour {
 	}
 
 	void GenerateCave() {
-        
-        int obstacleFreq = 2;
+
+        int obstacleFreq = obstacleMinFreq;
+        bool onGap = false;
+
 
 		for (int y = 0; y < tunnelDepth; y+=5) {
 
             float x = Mathf.PerlinNoise(y/25f, 0) * 6f;
             float z = Mathf.PerlinNoise(y/25f, 0) * 6f;
-
-           // float x = 0;
-           // float z = 0;
+            System.Random rand = new System.Random();
 
             //Create Walls
             Instantiate (caveTorus, new Vector3 (x, y, z), Quaternion.identity);
@@ -52,8 +67,31 @@ public partial class CaveGenerator : MonoBehaviour {
             //Create Obstacles
             if (obstacleFreq == 0)
             {
-                ObstacleGenerate(x, y, z);
-                obstacleFreq = 2;
+                //Instantiate object of end gap
+                if (!onGap)
+                {
+                    ObstacleGenerate(x, y, z);
+                    if (rand.Next(100) <= doubleChance) ObstacleGenerate(x, y, z);
+
+                } else
+                {
+                    Debug.Log("gap off");
+                    onGap = false;
+                }
+
+                //Gap or obstacle countdown
+                if (rand.Next(100) <= gapChance)
+                {
+                    Debug.Log("on gap");
+                    onGap = true;
+                    obstacleFreq = Random.Range(obstacleMaxFreq, maxGapLength);
+                }
+                else
+                {
+                    Debug.Log("on obstacle");
+                    obstacleFreq = Random.Range(obstacleMaxFreq, obstacleMinFreq);
+                }
+
             } else
             {
                 obstacleFreq -= 1;
