834162027d5459316b47adace864e0e2ec65e169
sahough <shough@trinity.edu>
2017-12-02 15:54:42 -0600

    added menus etc

---
+++ Assets/Scripts/Transitions/MainMenuPlay.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuPlay : MonoBehaviour {
+
+	public void loadProverbScene() {
+		SceneManager.LoadScene("ProverbScene");
+	}
+
+}
