f83bdf01e59043bfa4aa118afbc5bb3aa1294c60
sahough <shough@trinity.edu>
2017-12-02 12:27:43 -0600

    Adding Tunnel creation

---
+++ Assets/Scripts/ObstacleGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class CaveGenerator : MonoBehaviour {
+
+	void ObstacleGenerate(float _x, float _y, float _z) {
+	}
+
+}
