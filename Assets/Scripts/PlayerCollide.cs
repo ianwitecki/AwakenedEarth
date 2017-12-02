5661707c52f9ef8585df0b2fe06a7f44d47adcbc
sahough <shough@trinity.edu>
2017-12-02 17:51:06 -0600

    added stuff

---
+++ Assets/Scripts/PlayerCollide.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCollide : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if(transform.position.y < 20)
+			SceneManager.LoadScene ("GameWin");
+		SceneManager.LoadScene ("GameOver");
+	}
+}
